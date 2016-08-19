6406db17229a568cf3db16d2943d052d57cb6a94
merkulovvs <mervic81@gmail.com>
2016-08-19 19:09:29 +0300

    module1 homework

---
+++ src/module1/FifthClass.java
@@ -0,0 +1,8 @@
+package module1;
+
+
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("My first program on Java ...");
+    }
+}
---
+++ src/module1/FourthClass.java
@@ -0,0 +1,7 @@
+package module1;
+
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("My first program on Java ...");
+    }
+}

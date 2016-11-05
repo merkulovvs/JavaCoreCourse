f17a8cf036cabff7e88f810c4743d69ed25a3830
merkulovvs <mervic81@gmail.com>
2016-11-06 00:09:53 +0200

    module2 homework MaxPositive problem is fixed. When all values in the array are negative, program outputs the message: "There are no positive values in this array"

--- src/module2/MaxPositiveOfDoubleArray.java
+++ src/module2/MaxPositiveOfDoubleArray.java
@@ -9,8 +9,18 @@ public class MaxPositiveOfDoubleArray {
     public static void main(String[] args) {
         double [] arrayOfDoubles = {3.7, -10.1258, 15.00217, -1.0344, 3.14, 9.0852, 5.552, 18.999999, -8.1128, 2.5};
         System.out.println("The array is: " + (Arrays.toString(arrayOfDoubles)));
-        double arrayMaxValue = max(arrayOfDoubles);
-        System.out.println("The maximum positive value in this array is: " + arrayMaxValue);
+        int negativeNumbers = 0;
+        for (double i: arrayOfDoubles) {
+            if (i < 0) negativeNumbers++;
+        }
+        if (negativeNumbers == arrayOfDoubles.length) {
+            System.out.println("There are no positive values in this array");
+        }
+
+        else {
+            double arrayMaxValue = max(arrayOfDoubles);
+            System.out.println("The maximum positive value in this array is: " + arrayMaxValue);
+        }
     }
 
     public static double max(double array[]) {
--- src/module2/MaxPositiveTest.java
+++ src/module2/MaxPositiveTest.java
@@ -3,11 +3,11 @@ package module2;
 import java.util.Arrays;
 
 /**
- * Created by merkulovvs on 11/3/2016.
+ * Created by merkulovvs on 11/5/2016.
  */
-public class MaxPositiveOfIntArray {
+public class MaxPositiveTest {
     public static void main(String[] args) {
-        int[] arrayOfInts = new int[]{23, 12, 1, 3, 8, -3, 5, 19, -12, 2};
+        int[] arrayOfInts = new int[]{-23, -12, -1, -3, -8, -3, -5, -19, -12, -2};
         System.out.println("The array is: " + (Arrays.toString(arrayOfInts)));
         int negativeNumbers = 0;
         for (int i : arrayOfInts) {
@@ -16,10 +16,9 @@ public class MaxPositiveOfIntArray {
 
         if (negativeNumbers == arrayOfInts.length) {
             System.out.println("There are no positive values in this array");
-        }
-        else {
+        } else {
             int arrayMaxPositiveValue = max(arrayOfInts);
-            System.out.println("The maximum positive value in this array is: " + arrayMaxPositiveValue);
+            System.out.println("The maximum positive values in this array is: " + arrayMaxPositiveValue);
         }
     }
     public static int max(int array[]) {
